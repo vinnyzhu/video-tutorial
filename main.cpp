b3f7643ecab6d798d67353c11d3bf88b621888f4
vinnyzhu <wenyupowerful@hotmail.com>
2016-04-13 13:53:27 -0700

    try to creat a make file

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"function.h"
+
+int main(){
+    print_hello();
+    std::cout<<"this is"<<std::endl;
+    std::cout<<"The factorial of 10 is"<<factorial(10)<<std::endl;
+    return 0;
+    }
